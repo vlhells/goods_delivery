f6da4d58a1acca0663d7856e1f3192e101e376f4
vlhells <92589272+vlhells@users.noreply.github.com>
2024-01-06 23:24:36 +0300

    Some changes

--- app/forms.py
+++ app/forms.py
@@ -2,47 +2,47 @@ from django import forms
 from .models import OrderStates
 
 class CreateOrderForm(forms.Form):
-    target_address = forms.CharField()
-    meeting_place = forms.CharField()
-    meeting_time = forms.CharField()
-    description = forms.CharField()
-    weight = forms.FloatField
-    length = forms.FloatField
-    width = forms.FloatField
-    height = forms.FloatField
-    item_sender_fname = forms.CharField()
+    target_address = forms.CharField(required=True)
+    meeting_place = forms.CharField(required=True)
+    meeting_time = forms.CharField(required=True)
+    description = forms.CharField(required=True)
+    weight = forms.FloatField(required=True)
+    length = forms.FloatField(required=True)
+    width = forms.FloatField(required=True)
+    height = forms.FloatField(required=True)
+    item_sender_fname = forms.CharField(required=True)
     item_sender_sname = forms.CharField()
-    item_sender_lname = forms.CharField()
-    item_receiver_fname = forms.CharField()
+    item_sender_lname = forms.CharField(required=True)
+    item_receiver_fname = forms.CharField(required=True)
     item_receiver_sname = forms.CharField()
-    item_receiver_lname = forms.CharField()
+    item_receiver_lname = forms.CharField(required=True)
 
 class EditOrderForm(forms.Form):
-    target_address = forms.CharField()
-    meeting_place = forms.CharField()
-    meeting_time = forms.CharField()
-    description = forms.CharField()
-    weight = forms.FloatField
-    length = forms.FloatField
-    width = forms.FloatField
-    height = forms.FloatField
-    item_sender_fname = forms.CharField()
+    target_address = forms.CharField(required=True)
+    meeting_place = forms.CharField(required=True)
+    meeting_time = forms.CharField(required=True)
+    description = forms.CharField(required=True)
+    weight = forms.FloatField(required=True)
+    length = forms.FloatField(required=True)
+    width = forms.FloatField(required=True)
+    height = forms.FloatField(required=True)
+    item_sender_fname = forms.CharField(required=True)
     item_sender_sname = forms.CharField()
-    item_sender_lname = forms.CharField()
-    courier_fname = forms.CharField()
+    item_sender_lname = forms.CharField(required=True)
+    courier_fname = forms.CharField(required=True)
     courier_sname = forms.CharField()
-    courier_lname = forms.CharField()
-    item_receiver_fname = forms.CharField()
+    courier_lname = forms.CharField(required=True)
+    item_receiver_fname = forms.CharField(required=True)
     item_receiver_sname = forms.CharField()
-    item_receiver_lname = forms.CharField()
+    item_receiver_lname = forms.CharField(required=True)
 
 class ChangeOrderStateForm(forms.Form):
-    order_state = forms.ChoiceField(choices=OrderStates)
+    order_state = forms.ChoiceField(choices=OrderStates, required=True)
     # Исключить состояния inProcess и done в зав. от.
     cancellation_comment = forms.CharField()
 
 class CourierForm(forms.Form):
-    first_name = forms.CharField()
+    first_name = forms.CharField(required=True)
     second_name = forms.CharField()
-    last_name = forms.CharField()
-    phone_number = forms.CharField()
+    last_name = forms.CharField(required=True)
+    phone_number = forms.CharField(required=True)
--- app/models.py
+++ app/models.py
@@ -3,17 +3,17 @@ import enum
 from abc import ABC
 
 class Item(models.Model):
-    description = models.CharField(max_length=256)
-    weight = models.FloatField
-    length = models.FloatField
-    width = models.FloatField
-    height = models.FloatField
+    description = models.CharField(max_length=256, null=False)
+    weight = models.FloatField(null=False)
+    length = models.FloatField(null=False)
+    width = models.FloatField(null=False)
+    height = models.FloatField(null=False)
 
 class Human(models.Model, ABC):
-    first_name = models.CharField(max_length=25)
-    second_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=16)
+    first_name = models.CharField(max_length=25, null=False)
+    second_name = models.CharField(max_length=60, null=True)
+    last_name = models.CharField(max_length=50, null=False)
+    phone_number = models.CharField(max_length=16, null=False)
 
 class ItemSender(Human):
     pass
@@ -25,15 +25,15 @@ class ItemReceiver(Human):
     pass
 
 class Order(models.Model):
-    state = models.CharField(max_length=25)
-    meeting_time = models.DateTimeField
-    meeting_place = models.CharField(max_length=85)
-    target_address = models.CharField(max_length=85)
-    cancellation_comment = models.CharField(max_length=256)
-    item = models.ForeignKey(Item, on_delete = models.CASCADE)
-    item_sender = models.ForeignKey(ItemSender, on_delete = models.DO_NOTHING)
-    courier = models.ForeignKey(Courier, on_delete = models.DO_NOTHING)
-    item_receiver = models.ForeignKey(ItemReceiver, on_delete = models.DO_NOTHING)
+    state = models.CharField(max_length=25, null=False)
+    meeting_time = models.DateTimeField(null=False)
+    meeting_place = models.CharField(max_length=85, null=False)
+    target_address = models.CharField(max_length=85, null=False)
+    cancellation_comment = models.CharField(max_length=256, null=True)
+    item = models.ForeignKey(Item, on_delete = models.CASCADE, null=False)
+    item_sender = models.ForeignKey(ItemSender, on_delete = models.DO_NOTHING, null=False)
+    courier = models.ForeignKey(Courier, on_delete = models.DO_NOTHING, null=True)
+    item_receiver = models.ForeignKey(ItemReceiver, on_delete = models.DO_NOTHING, null=False)
 
 class OrderStates(enum.Enum):
     new = 'Новая'
